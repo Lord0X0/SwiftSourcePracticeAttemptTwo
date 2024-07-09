f358f20f17fd57d16a2ec7cc42b4da1b7ad6682b
0X0 <zachariahhornes1@yahoo.com>
2024-07-09 17:35:38 -0500

    Initial Commit

---
+++ SwiftSourcePracticeAttempt2/SwiftSourcePracticeAttempt2App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftSourcePracticeAttempt2App.swift
+//  SwiftSourcePracticeAttempt2
+//
+//  Created by Zachariah Hornes on 7/9/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftSourcePracticeAttempt2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
