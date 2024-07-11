368c13e70e0fdc5cb77fee46a7fba61a4a7aadc2
0X0 <zachariahhornes1@yahoo.com>
2024-07-11 11:06:26 -0500

    add shield emblem

---
+++ SwiftSourcePracticeAttempt2/HomeScreenCommitPractice .swift
@@ -0,0 +1,20 @@
+//
+//  HomeScreenCommitPractice .swift
+//  SwiftSourcePracticeAttempt2
+//
+//  Created by Zachariah Hornes on 7/10/24.
+//
+
+import SwiftUI
+
+struct HomeScreenCommitPractice_: View {
+    var body: some View {
+        Text("Main Screen")
+        Image(systemName: "shield")
+            .font(.largeTitle)
+    }
+}
+
+#Preview {
+    HomeScreenCommitPractice_()
+}
